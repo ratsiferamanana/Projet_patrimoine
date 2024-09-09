f7a30656928ce342c9516729b4ee7e0904903735
TSIFERANA <hei.jimmy.5@gmail.com>
2024-09-09 16:37:16 +0300

    fix:vite.config.js

--- UI/vite.config.js
+++ UI/vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // ou vue si vous utilisez Vue
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3500',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-    },
-  },
-});
+})
